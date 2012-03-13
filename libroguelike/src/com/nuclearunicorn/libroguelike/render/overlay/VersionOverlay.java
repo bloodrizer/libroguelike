31db50ec21285b010c24ee16e132b4231741e6e9
bloodrizer <cheshire.panther@gmail.com>
2012-03-13 09:25:22 +0300

    added version overlay

--- libroguelike/src/com/nuclearunicorn/libroguelike/render/overlay/VersionOverlay.java
+++ libroguelike/src/com/nuclearunicorn/libroguelike/render/overlay/VersionOverlay.java
@@ -5,15 +5,23 @@
 
 package com.nuclearunicorn.libroguelike.render.overlay;
 
+import com.nuclearunicorn.libroguelike.render.WindowRender;
+import org.newdawn.slick.Color;
+
 /**
  *
  * @author Administrator
  */
 public class VersionOverlay {
+
+    //to lazy to implement this shit
+    private static final String CLIENT_VER = "0.1.0";
+    private static final String BUILD_NUMBER = "1000";
+
     public static void render(){
-        /*OverlaySystem.ttf.drawString(5, WindowRender.get_window_h() - 18,
-                "Ver."+Io.CLIENT_VER +
-                "("+Io.PROTO_VER+")",
-        Color.white);*/
+        OverlaySystem.ttf.drawString(WindowRender.get_window_w() - 110, 30,
+                "Ver."+CLIENT_VER +
+                "("+BUILD_NUMBER+")",
+        Color.white);
     }
 }
